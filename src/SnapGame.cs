c1c5880b547b0680cabd8939b7d194719eca7533
wilsonto97 <wilsonto1997@github.com>
2018-03-12 14:59:26 +0800

    new commit: change the text

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -46,14 +46,14 @@ namespace CardGames
 		private static void DrawGame(Snap myGame)
 		{
 			SwinGame.DrawBitmap ("cardsBoard.png", 0, 0);
-
+			SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
 			// Draw the top card
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White,"GameFont", 0, 10);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White,"GameFont", 0, 35);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White,"GameFont", 0, 60);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
@@ -81,11 +81,13 @@ namespace CardGames
         public static void Main()
         {
             //Open the game window
+
             SwinGame.OpenGraphicsWindow("Snap!", 860, 500);
 
 			//Load the card images and set their cell details
             LoadResources();
-            
+
+
 			// Create the game!
 			Snap myGame = new Snap ();
 
